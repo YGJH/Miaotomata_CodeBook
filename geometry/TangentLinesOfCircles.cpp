d2d2dad9e06c6b6db3c1f239bef20a2fede6d43e
GU7432 <01257109@mail.ntou.edu.tw>
2024-09-27 00:34:13 +0800

    edit

--- geometry/TangentLinesOfCircles.cpp
+++ geometry/TangentLinesOfCircles.cpp
@@ -1,13 +1,13 @@
 vector<Line> CircleTangent(Cir c1, Cir c2, int sign1) {
     // sign1 = 1 for outer tang, -1 for inter tang
     vector<Line> ret;
-    double d_sq = abs2(c1.o - c2.o);
+    ld d_sq = abs2(c1.o - c2.o);
     if (sgn(d_sq) == 0) return ret;
-    double d = sqrt(d_sq);
+    ld d = sqrt(d_sq);
     Pt v = (c2.o - c1.o) / d;
-    double c = (c1.r - sign1 * c2.r) / d;
+    ld c = (c1.r - sign1 * c2.r) / d;
     if (c * c > 1) return ret;
-    double h = sqrt(max(0.0, 1.0 - c * c));
+    ld h = sqrt(max(0.0, 1.0 - c * c));
     for (int sign2 = 1; sign2 >= -1; sign2 -= 2) {
         Pt n = Pt(v.x * c - sign2 * h * v.y, v.y * c + sign2 * h * v.x);
         Pt p1 = c1.o + n * c1.r;
