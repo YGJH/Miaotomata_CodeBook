710e8aecde5f09f5560adb4439fe1138c94a0a87
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2024-10-04 02:47:49 +0800

    update manual partition point and compile command

--- basic/godcode.cpp
+++ basic/godcode.cpp
@@ -1,2 +1,3 @@
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+編譯指令: g++ -std=c++20 -w -Wfatal-errors -Wall -Wshadow -fsanitize=undefined
--- binarysearch/二分搜.cpp
+++ binarysearch/二分搜.cpp
@@ -29,3 +29,21 @@ int m = *ranges::partition_point(views::iota(0LL,(int)1e9+9),[&](int a){
 //111111100000000000
 //搜左邊數過來第一個 0
 //都是 1 會回傳 last
+
+int partitionpoint(int L,int R,function<bool(int)> chk){
+	int l = L,r = R-1;
+	while(r - l > 10){
+		int m = l + (r-l)/2;
+		if(chk(m)) l = m;
+		else r = m;
+	}
+    int m = l;
+    while(m <= r){
+        if(!chk(m)) break;
+        ++m;
+    } 
+	if(!chk(m)) return m;
+	else return R;
+}
+
+//手工
