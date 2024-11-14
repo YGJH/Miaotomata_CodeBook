49bdf35bf4f4b6e3e3ea61b142506ce18fd4fa2d
asd7766zxc <asd7766zxc@gmail.com>
2024-11-14 23:25:14 +0800

    upd godcode.cpp

--- basic/godcode.cpp
+++ basic/godcode.cpp
@@ -1,3 +1,21 @@
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 編譯指令: g++ -std=c++20 -w -Wfatal-errors -Wall -Wshadow -fsanitize=undefined
+
+mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int lb, int ub)
+{ return uniform_int_distribution<int>(lb, ub)(gen); }
+
+#define SECs ((double)clock() / CLOCKS_PER_SEC)
+
+struct KeyHasher {
+	size_t operator()(const Key& k) const {
+		return k.first + k.second * 100000;
+}	};
+typedef unordered_map<Key,int,KeyHasher> map_t;
+
+__builtin_popcountll    // 二進位有幾個1 (記得這是long long)
+__builtin_clzll         // 左起第一個1之前0的個數
+__builtin_parityll      // 1的個數的奇偶性
+__builtin_mul_overflow(a,b,&h) // a*b是否溢位,h = a * b;
+__builtin_add_overflow(a,b,&h) 
