7d5ad4e0fe8601e137548f7db1fb9d7370b247ed
GU7432 <01257109@mail.ntou.edu.tw>
2025-01-15 21:10:21 +0800

    fix BIT lowerbound

--- dataStructure/fenwickTree.cpp
+++ dataStructure/fenwickTree.cpp
@@ -1,24 +1,31 @@
-struct fenwick{
-	#define lowbit(x) (x&-x)
+struct fenwick {
+    // [0, n]
+	#define lowbit(x) (x & -x)
 	int n;
-	vector<int> v;
-	fenwick(int _n) : n(_n+1),v(_n+2){}
-	void add(int x,int u){
-		++x;
-		for(;x < n; x += lowbit(x)) v[x] += u;
+	vector<i64> v;
+	fenwick(i32 _n) : n(_n + 1), v(_n + 2, 0) {}
+	void _add(i32 x, i64 u){
+		for(;x <= n; x += lowbit(x)) v[x] += u;
 	}
-	int qry(int x){
-		++x; int ret = 0;
+	i64 _qry(i32 x){
+		int ret = 0;
 		for(; x ; x -= lowbit(x)) ret += v[x];
 		return ret;
 	}
-	int qry(int l,int r) { return qry(r) - qry(l-1); }
-	int kth(int k){ // lower_bound(k)
-		int x = 0; --k;
-		for(int i = (1<<__lg(n)); i;i >>= 1){
-			if(x + i <= n and k >= v[x + i]) x += i; k -= v[x];
-		}
-		return x;
-	}
-
+    i32 _lowerbound(i64 k) {
+        i64 sum = 0;
+        i32 p = 0;
+        for (i32 i = (1 << __lg(n)); i; i >>= 1) {
+            i32 nxt = p + i;
+            if (nxt <= n && sum + v[nxt] < k) {
+                sum += v[nxt];
+                p = nxt;
+            }
+        }
+        return p + 1;
+    }
+    void add(i32 x, i64 v) { _add(x + 1, v); }
+    i64 qry(i32 x) { return _qry(x + 1); }
+	i64 qry(i32 l,i32 r) { return qry(r) - qry(l - 1); }
+    i32 lower_bound(i64 k) { return _lowerbound(k) - 1; }
 };
