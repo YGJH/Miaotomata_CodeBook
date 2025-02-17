bbea06f3e885e1b6b094ba02f767ac0e98651200
GUaN <152492391+GU7432@users.noreply.github.com>
2025-02-17 23:29:19 +0800

    edit excrt.cpp

--- math/excrt.cpp
+++ math/excrt.cpp
@@ -1,50 +1,33 @@
-typedef __int128 ll;
-void exgcd(ll a,ll b,ll &g,ll &x,ll &y) {
-    if (b == 0) {
-        g = a;
-        x = 1;
-        y = 0;
-        return;
-    }
-    exgcd(b,a%b,g,y,x);
-    y-=(a/b)*x;
+i128 exgcd(i128 a, i128 b, i128 &x, i128 &y){
+    if (b == 0) return x=1, y=0, a;
+    int d = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
 }
-bool flag = false;
-ll a1,a2,n1,n2;
-ll abs(ll x) {
-    return x>0?x:-x;
-} 
-void china() {
-    ll d = a2 - a1;
-    ll g,x,y;
-    exgcd(n1,n2,g,x,y);
-    if (d % g == 0) {
-        x = ((x*d/g)%(n2/g)+(n2/g))%(n2/g);
-        a1 = x*n1 + a1;
-        n1 = (n1*n2)/g;
-    }
-    else 
-        flag = true;
-}
-int n;
-long long as[100001];  //算式答案 x
-long long ns[100001];  //模數 MOD
-ll realchina() {
-    a1 = as[0];
-    n1 = ns[0];
-    for (ll i = 1;i<n;i++) {
-        a2 = as[i];
-        n2 = ns[i];
+// as -> 算式答案 x
+// ns -> 模數 MOD
+i128 CRT(vector<i64> as, vector<i64> ns) {
+    i32 n = as.size();
+    i128 a1, a2, n1, n2;
+    bool flag = false;
+    auto china = [&]() {
+        i128 d = a2 - a1;
+        i128 x, y;
+        i128 g = exgcd(n1, n2, x, y);
+        if (d % g == 0) {
+            x = ((x * d / g) % (n2 / g) + (n2 / g)) % (n2 / g);
+            a1 = x * n1 + a1;
+            n1 = (n1 * n2) / g;
+        } else {
+            flag = true;
+        }
+    };
+
+    a1 = as[0], n1 = ns[0];
+    for (i32 i = 1; i < n; i++) {
+        a2 = as[i], n2 = ns[i];
         china();
-        if (flag)
-            return -1;
+        if (flag) return -1;
     }
     return a1;
-}
-int main() { 
-    cin>>n;
-    flag = false;
-    for (ll i = 0;i<n;i++)
-        cin>>ns[i]>>as[i];
-    cout<<(long long)realchina()<<endl; 
 }
