16ceceb49d04f5c81c43664709391fe445c84ca0
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2025-02-27 21:45:24 +0800

    Create Dinic_1.cpp

---
+++ flow/Dinic_1.cpp
@@ -0,0 +1,50 @@
+template<class Cap>
+struct Flow {
+    struct Edge { int v; Cap w; int rev; };
+    vector<vector<Edge>> G;
+    int n;
+    Flow(int n) : n(n), G(n) {}
+    void addEdge(int u, int v, Cap w) {
+        G[u].push_back({v, w, (int)G[v].size()});
+        G[v].push_back({u, 0, (int)G[u].size() - 1});
+    }
+    vector<int> dep;
+    bool bfs(int s, int t) {
+        dep.assign(n, 0);
+        dep[s] = 1;
+        queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int u = que.front(); que.pop();
+            for (auto [v, w, _] : G[u])
+                if (!dep[v] and w) {
+                    dep[v] = dep[u] + 1;
+                    que.push(v);
+                }
+        }
+        return dep[t] != 0;
+    }
+    Cap dfs(int u, Cap in, int t) {
+        if (u == t) return in;
+        Cap out = 0;
+        for (auto &[v, w, rev] : G[u]) {
+            if (w and dep[v] == dep[u] + 1) {
+                Cap f = dfs(v, min(w, in), t);
+                w -= f;
+                G[v][rev].w += f;
+                in -= f;
+                out += f;
+                if (!in) break;
+            }
+        }
+        if (in) dep[u] = 0;
+        return out;
+    }
+    Cap maxFlow(int s, int t) {
+        Cap ret = 0;
+        while (bfs(s, t)) {
+            ret += dfs(s, inf<Cap>, t);
+        }
+        return ret;
+    }
+};
