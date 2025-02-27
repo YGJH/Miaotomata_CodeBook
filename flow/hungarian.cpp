90c6d1409da49760ddfc02902e321c9d818d3ce6
ZIhan <73235000+zihan0221@users.noreply.github.com>
2025-02-27 21:43:00 +0800

    Update hungarian.cpp

--- flow/hungarian.cpp
+++ flow/hungarian.cpp
@@ -1,22 +1,35 @@
-bool dfs(int u){
-    for(int i : edge[u]){
-        if(!vis[i]){ // 有連通且未拜訪
+struct hungarian {
+    int L, R;
+    vector<bool> vis;
+    vector<int> match;
+    vector<vector<int>> E;
+    hungarian(int l, int r): //左邊有幾個，右邊有幾個
+    L(l), R(r),
+    vis(l+r+1),
+    match(l+r+1,-1),
+    E(l+r+1){}
+    void add_edge(int l, int r){//左側第幾個(1-base) ,右側第幾個(1-base)
+        r = L+r;
+        E[l].push_back(r);
+        E[r].push_back(l);
+    }
+    bool dfs(int u){
+        for(int i : E[u]){
+            if(vis[i]) continue; // 有連通且未拜訪
             vis[i] = true; // 紀錄是否走過
-            if(match[i]==-1 || dfs(match[i])){  
+            if(match[i] == -1 || dfs(match[i])){  
                 match[i] = u; match[u] = i; // 紀錄匹配
                 return true;
             }
         }
+        return false;
     }
-    return false;
-}
-int hungarian(){
-    int ans = 0;
-    memset(match, -1, sizeof(match));
-    for(int i = 1 ;i <= lhs; i++){
-        // 記得每次使用需清空vis陣列
-        memset(vis, 0, sizeof(vis));
-        if(dfs(i)) ans++;
+    int solve(){
+        int ans = 0;
+        for(int i = 1; i <= L; i++){
+            fill(all(vis),0);
+            if(dfs(i)) ans++;
+        }
+        return ans;
     }
-    return ans;
-}
+};
