628c1336fe0e98e647637bdfed4bf59d87cf6a16
Charles <jihuty@gmail.com>
2025-10-18 11:57:54 +0800

    Refactor z_value_pal to use std::string and std::vector 你媽死了
    Refactor z_value_pal function to use string and vector.

--- string/馬拉車.cpp
+++ string/馬拉車.cpp
@@ -1,15 +1,21 @@
-//以每個字元為中心的最長迴文長度
-//abc -> @a@b@c
-void z_value_pal(char* s, int len, int* z) {
-    len = (len << 1) + 1;
-    for (int i = len - 1; i >= 0; i--)
-        s[i] = i & 1 ? s[i >> 1] : '@';
-    z[0] = 1;
-    for (int i = 1, l = 0, r = 0; i < len; i++) {
-        z[i] = i < r ? min(z[l + l - i], r - i) : 1;
-        while (i - z[i] >= 0 && i + z[i] < len && s[i - z[i]] == s[i + z[i]])
-            ++z[i];
-        if (i + z[i] > r)
-            l = i, r = i + z[i];
+void z_value_pal(const string &input, vector<int> &z)
+{
+    // 幹你娘Miaotomata 你是不會用動態分配的陣列喔 他媽的害我一直runtime error
+    int len = input.size();
+    string s; s.reserve((len << 1) + 1);
+    for (int i = 0; i < len; ++i)
+    {
+        s.push_back('@');
+        s.push_back(input[i]);
+    }
+    s.push_back('@');
+
+    int n = s.size();
+    z.assign(n, 1);
+    for (int i = 1, l = 0, r = 0; i < n; ++i)
+    {
+        z[i] = (i < r ? min(z[l + l - i], r - i) : 1);
+        while (i - z[i] >= 0 && i + z[i] < n && s[i - z[i]] == s[i + z[i]]) ++z[i];
+        if (i + z[i] > r) l = i, r = i + z[i];
     }
 }
