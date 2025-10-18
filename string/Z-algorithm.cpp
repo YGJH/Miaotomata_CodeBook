26ecda69bf414875677017a9bde81b5bd1cd09e9
Charles <jihuty@gmail.com>
2025-10-18 13:18:12 +0800

    Update Z-algorithm.cpp

--- string/Z-algorithm.cpp
+++ string/Z-algorithm.cpp
@@ -1,14 +1,12 @@
-//z[i] = s 跟 s[i..n-1] 的最長真共同前綴長度 // z[0] = 0
-vector<int> zfunc(string &s){ 
-	int n = s.size();
-	vector<int> z(n);
-	for(int i = 1,l = 0,r = 0; i < n;++i){
-		if(i <= r && z[i - l] < r - i + 1) z[i] = z[i - l];
-		else {
-			z[i] = max(0LL,r - i + 1);
-			while(i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
-		}
-		if(i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
-	}
-	return z;
+// Z-function: z[0]=0 (或定義為n也可，但常設0)，對 i>=1 計算 S 與 S[i:] 的 LCP 長度
+vector<int> z_function(const string &s) {
+    int n = (int)s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; ++i) {
+        if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        if (i + z[i] - 1 > r) { l = i; r = i + z[i] - 1; }
+    }
+    return z;
 }
